45f01e9c0394535d1c949b9c53f0098bc1733bbb
naammaann31 <namanrtrivedi@gmail.com>
2026-08-13 01:34:54 +0530

    asdf

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["xlsx", "xlsx-js-style"],
-  allowedDevOrigins: ['192.168.1.30'],
+  allowedDevOrigins: ['192.168.1.50'],
 };
 
 export default nextConfig;
