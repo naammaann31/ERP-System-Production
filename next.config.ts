e017f2e53d4489d790d3db4e175a72af3e43a3c5
Hamzah <hamzah.2004saiyed@gmail.com>
2026-08-15 00:10:26 +0530

    Update Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["xlsx", "xlsx-js-style"],
-  allowedDevOrigins: ['192.168.1.50'],
+  allowedDevOrigins: ["192.168.1.50", "192.168.1.30"],
 };
 
 export default nextConfig;
